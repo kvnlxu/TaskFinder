4e3d89d4d09698ff4306968c134afe19db0658bb
Brandon Diep <108596840+BrandonTDiep@users.noreply.github.com>
2025-11-20 19:54:46 -0800

    update task controller, model task, and task repo (#5)

--- src/main/java/org/example/taskfinderapp/repository/TaskRepository.java
+++ src/main/java/org/example/taskfinderapp/repository/TaskRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findByCustomerId(Long customerId);
     List<Task> findByContractorId(Long contractorId);
+    List<Task> findByStatus(String status);
 }
